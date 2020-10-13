35b724f1a5aa98e56481dfcad77d73c9f52a2653
Eduardo Baeza <eduardo.baeza@yahoo.com>
2020-10-13 14:28:24 -0500

    My each method test block, array and object tests

---
+++ spec/my_all.rb
@@ -0,0 +1,11 @@
+require '../script.rb'
+
+include Enumerable do
+  describe '#my_each' do
+    let(:ary) {[1, 2, 3, 4, 5]}
+
+    it 'When block not given' do
+      expect(ary.my_each(&block)).to eql(ary.my_each(&block))
+    end
+  end
+end
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -3,6 +3,7 @@ require '../script.rb'
 RSpec.describe Enumerable do
   describe '#my_each' do
     let(:ary) { [1, 2, 3, 4, 5] }
+    let(:range) { (1..10) }
 
     it 'when block not given returns Enumerator' do
         expect(ary.my_each).to be_an Enumerator
@@ -10,6 +11,10 @@ RSpec.describe Enumerable do
 
     it 'when block given' do
         expect(ary.my_each {|x| x}).to be_an Array
+    end
+
+    it 'when a range given' do
+        expect(range.my_each {|x| x}).to be_an Object
     end
   end
 end
